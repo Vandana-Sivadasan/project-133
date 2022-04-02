d8415f27b514c58c16a2fad77c2d6f96cda15661
Vandana Sivadasan <51973070+Vandana-Sivadasan@users.noreply.github.com>
2022-04-01 19:58:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+img="";
+function preload(){
+    img=loadImage('tv.jpg');
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+}
+function draw() {
+    image(img,0,0,640,420);
+    fill(255,0,0);
+    text("Tv",45,75);
+    noFill();
+    stroke(255,0,0);
+   rect(30,60,450,350);
+}
